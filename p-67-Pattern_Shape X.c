e77c7e9d667f0ba5645b2cab0df59873b2679294
Amitdole3333 <amitdole3333@gmail.com>
2025-03-20 06:49:33 +0000

    p-67

---
+++ p-67-Pattern_Shape X.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+   int n;
+    scanf("%d",&n);
+  if(n<=1){
+      printf("Shape Not Possible");
+  }    
+    if(n>=2){
+        if(n%2==0){
+        n=n+1;
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+          if(i==j || j==n+1-i){
+              printf("*");
+          }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    }
+    
+    
+    return 0;
+}
