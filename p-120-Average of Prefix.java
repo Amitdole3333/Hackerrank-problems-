1c95f6753d33413d8a1e46d16122515de91a05af
Amitdole3333 <amitdole3333@gmail.com>
2025-05-12 13:51:30 +0000

    p-120

--- p-120-Average of Prefix.java
+++ p-120-Average of Prefix.java
@@ -1,1 +1,17 @@
-Average of Prefix
+class Solution {
+  public:
+    vector<int> prefixAvg(vector<int> &arr) {
+        // code here
+        int x=arr.size();
+        int avg[x];
+        for(int i=0;i<x;i++){
+            avg[i]=arr[i];
+        }
+        int sum=0;
+        for (int i=0;i<x;i++){
+           sum=sum+avg[i];
+           arr[i]=sum/(i+1);
+        }
+        return arr;
+    }
+};
